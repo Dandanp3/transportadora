e0071931da8508a4cffd1d4d81cbc272f7c2bab4
Dandanp3 <daniel.pyoliverp@gmail.com>
2026-09-15 13:54:38 -0300

    feat: entrega

--- Entrega.java
+++ Entrega.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+
 public class Entrega {
+    private Cliente cliente;
+    private ArrayList<Produto> produtos;
+    private String status;
+    private double valorFrete;
+
+    public Entrega(Cliente cliente, ArrayList<Produto> produtos, String status, double valorFrete) {
+        this.cliente = cliente;
+        this.produtos = produtos;
+        this.status = status;
+        this.valorFrete = valorFrete;
+    }
+
+    //getters e setters
     
 }
+
--- Produto.java
+++ Produto.java
@@ -1,3 +1,21 @@
 public class Produto {
-    
+    private String nome;
+    private double peso;
+    private double preco;
+
+    public Produto(String nome, double peso, double preco) {
+        this.nome = nome;
+        this.peso = peso;
+        this.preco = preco;
+    }
+
+    //getters e seters
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public double getPeso() { return peso; }
+    public void setPeso(double peso) { this.peso = peso; }
+
+    public double getPreco() { return preco; }
+    public void setPreco(double preco) { this.preco = preco; }
 }
